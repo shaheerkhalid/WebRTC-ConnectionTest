f9784ebd9d5e471e03e0dce6e4bc36c399d78105
shaheerkhalid <shaheerkhalid12@gmail.com>
2020-02-10 23:32:39 +0500

    button setting

--- test.js
+++ test.js
@@ -1,19 +1,35 @@
+const stunServer = {"url": "stun:bturn2.xiasdfdsfrsys.com"};
+const turnServer = {
+              url: 'turn:bturn2.xirasdfsasys.com:80?transport=udp',
+              username: 'ymms7Hxn1MyMr0YO5S3MM5BkwgARPkN6TxQOZiXRfrsFK2lRGQbLK8Qt4DHlGry7AAAAAF49v1hzaGFoZWVya2hhbGlk',
+              credential: 'fd212486-49e2-11ea-8456-9646de0e6ccd'
+  };
+
 const mediaStreamConstraints = {
   video: true,
   audio: true
 };
 
+let localStream;
 
 function gotLocalMediaStream(mediaStream) {
   localStream = mediaStream;
+  testStun.disabled = false;
+  testTurn.disabled = false;
   // localVideo.srcObject = mediaStream;
 }
 
 // Handles error by logging a message to the console with the error message.
 function handleLocalMediaStreamError(error) {
-  console.log('navigator.getUserMedia error: ', error);
+  printInfo('navigator.getUserMedia error: '+ error);
+  testStun.disabled = true;
 }
 
+
+function printInfo(description){
+  console.log(description);
+  output.innerHTML = description;
+}
 
 function checkTurnOrStun(turnConfig, timeout){ 
     return new Promise(function(resolve, reject){
@@ -26,14 +42,12 @@ function checkTurnOrStun(turnConfig, timeout){
           resolve(false);
           promiseResolved = true;
       }, timeout || 5000);
-      navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
-        .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+
       var promiseResolved = false
         , myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection   //compatibility for firefox and chrome
         , pc = new myPeerConnection({iceServers:[turnConfig]})
         , noop = function(){};
       window.remoteConnection = pc;
-      
       var dataChannel = pc.createDataChannel("dataChannel", {reliable: false});    //create a bogus data channel
 
       pc.onicecandidate = function (event) {
@@ -65,24 +79,46 @@ function checkTurnOrStun(turnConfig, timeout){
   }
 
 
-  testStun.onclick = checkTurnOrStun({"url": "stun:bturn2.xiasdfdsfrsys.com"}).then(function(result){
-      console.log(
-      result ? 'YES, Server active as '+result : 'NO, server not active');
-
-      output.innerHTML = '<p>'+ result +'</p>';  
-
-
-  }).catch(console.error.bind(console));
   
-  testTurn.onclick = checkTurnOrStun({
-              url: 'turn:bturn2.xirasdfsasys.com:80?transport=udp',
-              username: 'ymms7Hxn1MyMr0YO5S3MM5BkwgARPkN6TxQOZiXRfrsFK2lRGQbLK8Qt4DHlGry7AAAAAF49v1hzaGFoZWVya2hhbGlk',
-              credential: 'fd212486-49e2-11ea-8456-9646de0e6ccd'
-  }).then(function(result){
-      console.log(
+  
+  // testTurn.onclick = checkTurnOrStun({
+  //             url: 'turn:bturn2.xirasdfsasys.com:80?transport=udp',
+  //             username: 'ymms7Hxn1MyMr0YO5S3MM5BkwgARPkN6TxQOZiXRfrsFK2lRGQbLK8Qt4DHlGry7AAAAAF49v1hzaGFoZWVya2hhbGlk',
+  //             credential: 'fd212486-49e2-11ea-8456-9646de0e6ccd'
+  // }).then(function(result){
+  //     printInfo(
+  //     result ? 'YES, Server active as '+result : 'NO, server not active');
+
+  //     output.innerHTML = '<p>'+ result ? 'YES, Server active as '+result : 'NO, server not active' +'</p>'; 
+  // }).catch(console.error.bind(console));
+
+  function preStart(){
+    testTurn.disabled = true;
+    testStun.disabled = true;
+    navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+    .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+
+  }
+
+
+  preStart();
+
+ function start(b_name){
+  
+  let server = null;
+   
+   
+    console.log(b_name);
+  if(b_name == "testStun"){
+     server = stunServer;
+   }else if(b_name == "testTurn"){
+     server = turnServer;
+   }
+   console.log(server);
+   checkTurnOrStun(server).then(function(result){
+    printInfo(
       result ? 'YES, Server active as '+result : 'NO, server not active');
+    }).catch(console.error.bind(console));
 
-      output.innerHTML = '<p>'+ result ? 'YES, Server active as '+result : 'NO, server not active' +'</p>'; 
-  }).catch(console.error.bind(console));
+ }
 
- 
