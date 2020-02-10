053d9ab6e91ded30985491bdb12ede50fa566dcc
shaheerkhalid <shaheerkhalid12@gmail.com>
2020-02-10 22:50:17 +0500

    add media access

--- test.js
+++ test.js
@@ -1,19 +1,18 @@
-function setChannelEvents(channel, channelNameForConsoleOutput) {
-  channel.onmessage = function (event) {
-      console.debug(channelNameForConsoleOutput, 'received a message:', event.data);
-  };
+const mediaStreamConstraints = {
+  video: true,
+  audio: true
+};
 
-  channel.onopen = function () {
-      channel.send('first text message over RTP data ports');
-  };
-  channel.onclose = function (e) {
-      console.error(e);
-  };
-  channel.onerror = function (e) {
-      console.error(e);
-  };
+
+function gotLocalMediaStream(mediaStream) {
+  localStream = mediaStream;
+  // localVideo.srcObject = mediaStream;
 }
 
+// Handles error by logging a message to the console with the error message.
+function handleLocalMediaStreamError(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
 
 
 function checkTurnOrStun(turnConfig, timeout){ 
@@ -27,30 +26,21 @@ function checkTurnOrStun(turnConfig, timeout){
           resolve(false);
           promiseResolved = true;
       }, timeout || 5000);
-  
+      navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+        .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
       var promiseResolved = false
         , myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection   //compatibility for firefox and chrome
         , pc = new myPeerConnection({iceServers:[turnConfig]})
         , noop = function(){};
+      window.remoteConnection = pc;
+      
       var dataChannel = pc.createDataChannel("dataChannel", {reliable: false});    //create a bogus data channel
-      
-
-
-      
-      setChannelEvents(dataChannel, 'offerer');
 
       pc.onicecandidate = function (event) {
           if (!event || !event.candidate) return;
           answerer && answerer.addIceCandidate(event.candidate);
       };
 
-      var mediaConstraints = {
-        optional: [],
-        mandatory: {
-            OfferToReceiveAudio: false, // Hmm!!
-            OfferToReceiveVideo: false // Hmm!!
-        }
-      };
 
       pc.createOffer(function(sdp){
         if(sdp.sdp.indexOf('typ relay') > -1){ // sometimes sdp contains the ice candidates...
@@ -66,40 +56,33 @@ function checkTurnOrStun(turnConfig, timeout){
             promiseResolved = 'STUN';
           if (turnConfig.url.indexOf('turn:')!==0) resolve('STUN');
         }
+
+
+
         else return;
       };
     });   
   }
 
 
-  checkTurnOrStun({"url": "stun:bturn2.xirsys.com"}).then(function(result){
+  testStun.onclick = checkTurnOrStun({"url": "stun:bturn2.xiasdfdsfrsys.com"}).then(function(result){
       console.log(
       result ? 'YES, Server active as '+result : 'NO, server not active');
+
+      output.innerHTML = '<p>'+ result +'</p>';  
+
+
   }).catch(console.error.bind(console));
   
-  checkTurnOrStun({
-              url: 'turn:bturn2.xirsys.com:80?transport=udp',
+  testTurn.onclick = checkTurnOrStun({
+              url: 'turn:bturn2.xirasdfsasys.com:80?transport=udp',
               username: 'ymms7Hxn1MyMr0YO5S3MM5BkwgARPkN6TxQOZiXRfrsFK2lRGQbLK8Qt4DHlGry7AAAAAF49v1hzaGFoZWVya2hhbGlk',
               credential: 'fd212486-49e2-11ea-8456-9646de0e6ccd'
   }).then(function(result){
       console.log(
       result ? 'YES, Server active as '+result : 'NO, server not active');
+
+      output.innerHTML = '<p>'+ result ? 'YES, Server active as '+result : 'NO, server not active' +'</p>'; 
   }).catch(console.error.bind(console));
 
  
-  
-
-
-//   checkTURNServer({
-//     url: 'stun:bturn2.xirsys.com',
-//     username: 'ymms7Hxn1MyMr0YO5S3MM5BkwgARPkN6TxQOZiXRfrsFK2lRGQbLK8Qt4DHlGry7AAAAAF49v1hzaGFoZWVya2hhbGlk',
-//     credential: 'fd212486-49e2-11ea-8456-9646de0e6ccd'
-// }).then(function(bool){
-//     console.log('is TURN server active? ', bool? 'yes':'no');
-// }).catch(console.error.bind(console));
-
-
-
-
-
-
