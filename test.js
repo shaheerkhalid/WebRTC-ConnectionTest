2bb038785a8b8e6ffc952d1fa05c3a9c132d11b2
shaheerkhalid <shaheerkhalid12@gmail.com>
2020-02-11 00:03:51 +0500

    media setting

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-const stunServer = {"url": "stun:bturn2.xiasdfdsfrsys.com"};
+const stunServer = {"url": "stun:bturn2.xirsys.com"};
 const turnServer = {
-              url: 'turn:bturn2.xirasdfsasys.com:80?transport=udp',
+              url: 'turn:bturn2.xirsys.com:80?transport=udp',
               username: 'ymms7Hxn1MyMr0YO5S3MM5BkwgARPkN6TxQOZiXRfrsFK2lRGQbLK8Qt4DHlGry7AAAAAF49v1hzaGFoZWVya2hhbGlk',
               credential: 'fd212486-49e2-11ea-8456-9646de0e6ccd'
   };
@@ -12,11 +12,75 @@ const mediaStreamConstraints = {
 
 let localStream;
 
+var video = document.querySelector('video');
+
+function grabWebCamVideo() {
+  console.log('Getting user media (video) ...');
+  navigator.mediaDevices.getUserMedia({
+    video: true
+  })
+  .then(gotStream)
+  .catch(function(e) {
+    alert('getUserMedia() error: ' + e.name);
+  });
+}
+
+
+
+function createConnection() {
+  dataChannelSend.placeholder = '';
+  var servers = null;
+  pcConstraint = null;
+  dataConstraint = null;
+  trace('Using SCTP based data channels');
+  // For SCTP, reliable and ordered delivery is true by default.
+  // Add localConnection to global scope to make it visible
+  // from the browser console.
+  window.localConnection = localConnection =
+      new RTCPeerConnection(servers, pcConstraint);
+  trace('Created local peer connection object localConnection');
+
+  sendChannel = localConnection.createDataChannel('sendDataChannel',
+      dataConstraint);
+  trace('Created send data channel');
+
+  localConnection.onicecandidate = iceCallback1;
+  sendChannel.onopen = onSendChannelStateChange;
+  sendChannel.onclose = onSendChannelStateChange;
+
+  // Add remoteConnection to global scope to make it visible
+  // from the browser console.
+  window.remoteConnection = remoteConnection =
+      new RTCPeerConnection(servers, pcConstraint);
+  trace('Created remote peer connection object remoteConnection');
+
+  remoteConnection.onicecandidate = iceCallback2;
+  remoteConnection.ondatachannel = receiveChannelCallback;
+
+  localConnection.createOffer().then(
+    gotDescription1,
+    onCreateSessionDescriptionError
+  );
+  startButton.disabled = true;
+  closeButton.disabled = false;
+}
+
+function sendData() {
+  var data = dataChannelSend.value;
+  sendChannel.send(data);
+  trace('Sent Data: ' + data);
+}
+
+
+
+
+
+
 function gotLocalMediaStream(mediaStream) {
   localStream = mediaStream;
   testStun.disabled = false;
   testTurn.disabled = false;
-  // localVideo.srcObject = mediaStream;
+  video.srcObject = mediaStream;
 }
 
 // Handles error by logging a message to the console with the error message.
@@ -24,6 +88,78 @@ function handleLocalMediaStreamError(error) {
   printInfo('navigator.getUserMedia error: '+ error);
   testStun.disabled = true;
 }
+
+
+function handleConnection(event) {
+  const peerConnection = event.target;
+  const iceCandidate = event.candidate;
+
+  if (iceCandidate) {
+    const newIceCandidate = new RTCIceCandidate(iceCandidate);
+    const otherPeer = getOtherPeer(peerConnection);
+
+    otherPeer.addIceCandidate(newIceCandidate)
+      .then(() => {
+        handleConnectionSuccess(peerConnection);
+      }).catch((error) => {
+        handleConnectionFailure(peerConnection, error);
+      });
+
+    console.trace(`${getPeerName(peerConnection)} ICE candidate:\n` +
+          `${event.candidate.candidate}.`);
+  }
+}
+
+
+function setSessionDescriptionError(error){
+
+}
+
+function setLocalDescriptionSuccess(des){
+
+}
+
+
+function createdOffer(description) {
+  console.trace(`Offer from localPeerConnection:\n${description.sdp}`);
+
+  console.trace('localPeerConnection setLocalDescription start.');
+  localPeerConnection.setLocalDescription(description)
+    .then(() => {
+      setLocalDescriptionSuccess(localPeerConnection);
+    }).catch(setSessionDescriptionError);
+
+  console.trace('remotePeerConnection setRemoteDescription start.');
+  remotePeerConnection.setRemoteDescription(description)
+    .then(() => {
+      setRemoteDescriptionSuccess(remotePeerConnection);
+    }).catch(setSessionDescriptionError);
+
+  console.trace('remotePeerConnection createAnswer start.');
+  remotePeerConnection.createAnswer()
+    .then(createdAnswer)
+    .catch(setSessionDescriptionError);
+}
+
+// Logs answer to offer creation and sets peer connection session descriptions.
+function createdAnswer(description) {
+  console.trace(`Answer from remotePeerConnection:\n${description.sdp}.`);
+
+  console.trace('remotePeerConnection setLocalDescription start.');
+  remotePeerConnection.setLocalDescription(description)
+    .then(() => {
+      setLocalDescriptionSuccess(remotePeerConnection);
+    }).catch(setSessionDescriptionError);
+
+  console.trace('localPeerConnection setRemoteDescription start.');
+  localPeerConnection.setRemoteDescription(description)
+    .then(() => {
+      setRemoteDescriptionSuccess(localPeerConnection);
+    }).catch(setSessionDescriptionError);
+}
+
+
+
 
 
 function printInfo(description){
@@ -48,6 +184,7 @@ function checkTurnOrStun(turnConfig, timeout){
         , pc = new myPeerConnection({iceServers:[turnConfig]})
         , noop = function(){};
       window.remoteConnection = pc;
+      remotePeerConnection = new myPeerConnection({iceServers:[turnConfig]});
       var dataChannel = pc.createDataChannel("dataChannel", {reliable: false});    //create a bogus data channel
 
       pc.onicecandidate = function (event) {
@@ -61,6 +198,29 @@ function checkTurnOrStun(turnConfig, timeout){
           promiseResolved = 'TURN'; 
           resolve(true);
         }
+
+        console.trace(`Offer from localPeerConnection:\n${sdp.sdp}`);
+
+        console.trace('localPeerConnection setLocalDescription start.');
+        pc.setLocalDescription(sdp)
+          .then(() => {
+            setLocalDescriptionSuccess(localPeerConnection);
+          }).catch(setSessionDescriptionError);
+
+        console.trace('remotePeerConnection setRemoteDescription start.');
+        pc.setRemoteDescription(sdp)
+          .then(() => {
+            setRemoteDescriptionSuccess(remotePeerConnection);
+          }).catch(setSessionDescriptionError);
+
+        console.trace('remotePeerConnection createAnswer start.');
+        remotePeerConnection.createAnswer()
+          .then(createdAnswer)
+          .catch(setSessionDescriptionError);
+
+
+
+
         pc.setLocalDescription(sdp, noop, noop);
       }, noop);    // create offer and set local description
       pc.onicecandidate = function(ice){  //listen for candidate events
